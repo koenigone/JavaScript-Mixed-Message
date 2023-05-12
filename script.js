8c1f4610ba631b16bfe602cb29560048f887ee7b
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-12 17:50:08 +0100

    added dark/lgiht mode switcher and a bot

---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.querySelector('body')
+const lightSwitch = document.querySelector('#lightSwitch');
+const darkSwitch = document.querySelector('#darkSwitch');
+const userInput = document.querySelector('#userInput');
+const submit = document.querySelector('#submit');
+const botReply = document.querySelector('#bot');
+
+
+darkSwitch.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'rgb(8, 11, 61)';
+    body.style.color = 'rgb(224, 224, 224)';
+    lightSwitch.style.display = 'block';
+    darkSwitch.style.display = 'none';
+});
+
+lightSwitch.addEventListener('click', ()=> {
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+    userInput.style.border = '1px solid black'
+    lightSwitch.style.display = 'none';
+    darkSwitch.style.display = 'block'
+});
+
+
+submit.addEventListener('click', ()=> {
+    const messages = ['Good morning!', 'How was your day?', 'Have you had your coffee yet?', 'seems like you are bored', 'So What now', 'Ok', 'lol', 'Get away', 'Nicee', 'GG', 'I dont know', 'keep going', 'any plans today?'];
+    const randomizer = Math.floor(Math.random() * messages.length);
+    const randomizedMessages = messages[randomizer];
+    if (userInput.value === 'hi') {
+        botReply.innerHTML = randomizedMessages;
+    };
+});
